2a87ef69c4a254315f9b5c8b102c98489425b15d
Abhishek Dubey <abhishek.25b01011146@abes.ac.in>
2026-07-23 11:54:17 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,16 @@
+console.log("Abhishek dubey");
+
+// write a function that takes two numbers as arguments and returns
+//  their sum
+function sum(a, b) {
+return a + b;
+}
+
+const add = (a,b) => {
+    return a + b;
+};
+const add2 = (a,b) => a + b;
+
+console.log(sum(5, 10));
+console.log(add(5, 10));
+console.log(add2(5, 10));
